86d87618e7514ac5d2da9195e4a98cdbfe4179d5
Narayan Bandodker <narayanband1356@gmail.com>
2024-04-13 00:18:51 +0530

    Fixed metadata passing to context Dummy replace Remove prompt example saying key name

--- ragchains/retrieval_qa.py
+++ ragchains/retrieval_qa.py
@@ -28,10 +28,15 @@ def print_me(inputs):
     return inputs
 
 
-def summarize_text_block(text: str, chat_model: BaseChatModel, text_id: str):
-    prompt = ChatPromptTemplate.from_template("The following content is an excerpt from an insurance report. Give a concise summary of the following content, retain important information. Also state the report title, if present: {data}")
-    summarize_chain = {"data": RunnablePassthrough()} | prompt | chat_model | StrOutputParser()
-    print("Summarizing text %s, \"%s\"..." % (text_id, text[:30]))
+def summarize_text_block(text_element: Text, chat_model: BaseChatModel, text_id: str):
+    text = text_element.text
+    # prompt = ChatPromptTemplate.from_template("The following content is an excerpt from an insurance report. Give a concise summary of the following content, retain important information. Also state the report title, if present: {data}")
+    # summarize_chain = {"data": RunnablePassthrough()} | prompt | chat_model | StrOutputParser()
+    # print("Summarizing text %s, \"%s\"..." % (text_id, text[:30]))
+    
+    # if "November 2, 2022" in text:
+    #     text = text.replace("November 2, 2022", "<Report Date> November 2, 2022")
+    
     return text  # summarize_chain.invoke(text)
 
 
@@ -42,9 +47,9 @@ def summarize_table(table_html: str, chat_model: BaseChatModel, table_id: str):
     return summarize_chain.invoke(table_html)
 
 
-def ingest_process(document_file, store: RetrievalStore, summarizer_model: BaseChatModel = None):
+def ingest_process(document_file, store: RetrievalStore, summarizer_model: BaseChatModel = None, cache_directory: str = None):
     print("Ingesting documents...")
-    parsed_doc = ingest_document(document_file, cache_directory=CACHE_DIR)
+    parsed_doc = ingest_document(document_file, cache_directory=cache_directory)
 
     retriever = store.retriever
     vectorstore = store.vectorstore
@@ -64,7 +69,7 @@ def ingest_process(document_file, store: RetrievalStore, summarizer_model: BaseC
             print("Saving text summaries...")
             summary_texts = [
                 Document(page_content=summarize_text_block(s, summarizer_model, t_id), metadata={id_key: t_id})
-                for t_id, s in zip(text_ids, texts)
+                for t_id, s in zip(text_ids, text_elements)
             ]
             retriever.vectorstore.add_documents(summary_texts)
 
@@ -94,26 +99,33 @@ def ingest_process(document_file, store: RetrievalStore, summarizer_model: BaseC
             txt_id = txt_elem.id
             if txt_id in text_ids:
                 continue
-            txt = str(txt_elem)
+            txt = summarize_text_block(txt_elem, summarizer_model, txt_id)
             m = txt_elem.metadata
             texts.append(txt)
             text_ids.append(txt_id)
             text_metadatas.append({
                 "page_number": m.page_number,
-                "file": m.filename
+                "file": m.filename,
+                "type": "text block"
             })
-        vectorstore.add_texts(texts, ids=text_ids)
+        vectorstore.add_texts(texts, ids=text_ids, metadatas=text_metadatas)
 
         # Tables
         table_elements = list(filter(lambda x: isinstance(x, Table), parsed_doc))
         tables = []
         table_ids = []  # [str(uuid.UUID(bytes=hashlib.sha256(t.encode()).digest()[:16])) for t in tables]
+        table_metadatas = []
         for txt, txt_id in zip(map(lambda x: x.metadata.text_as_html, table_elements), map(lambda e: e.id, table_elements)):
             if txt_id in table_ids:
                 continue
             tables.append(txt)
             table_ids.append(txt_id)
-        vectorstore.add_texts(tables, ids=table_ids)
+            table_metadatas.append({
+                "page_number": m.page_number,
+                "file": m.filename,
+                "type": "html table"
+            })
+        vectorstore.add_texts(tables, ids=table_ids, metadatas=table_metadatas)
 
     print("Retriever loading done")
 
@@ -121,21 +133,28 @@ def ingest_process(document_file, store: RetrievalStore, summarizer_model: BaseC
 
 
 def format_docs(docs: List[Document]):
-    return "\n\n".join(["Page number: %s\n%s" % (d.metadata.get("page_number", "Unknown"), d.page_content) for d in docs])
+    return "------\n\n".join([
+        "Page number: %s\nType: %s\n%s" % (
+            d.metadata.get("page_number", "Unknown"),
+            d.metadata.get("type", "text"),
+            d.page_content
+        )
+        for d in docs
+    ])
 
 
 def retrieval_chain(retriever: BaseRetriever, chat_model: BaseChatModel):
-    template = """Fetch requested information in the following context and answer directly, without explanations. If not found, say "Unknown". If field is present but blank or no value, mention "N/A".
+    template = """Fetch requested information in the following context and answer directly, without explanations and without repeating the request. If not found, say "Unknown". If field is present but blank or no value, mention "N/A".
     Example:
         Context: ```The cost was $22.4 and there were 10 employees.```
         Request: How much was the cost?
-        AI: Cost: $22.4
+        AI: $22.4
         Request: How many employees?
-        AI: Employees: 10
+        AI: 10
 
     Tips:
     - The context is like a letter, with a Date mentioned at the beginning of page 1. This can be used as the report date.
-    - If the report is not "first report", then "first report date" should be "N/A" as this can't be known.
+    - If the report is not "first report" or "preliminary", then "first report date" should be "N/A" as this can't be known.
 
     Start.
 
@@ -143,7 +162,7 @@ def retrieval_chain(retriever: BaseRetriever, chat_model: BaseChatModel):
     ```
     {context}
     ```
-    Request: {question}
+    Request (final): {question}
     AI: """
     prompt = ChatPromptTemplate.from_template(template)
 
